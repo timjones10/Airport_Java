c858c128ccf222fd040e1889a5cbcebf18539f96
Tim Jones <timjones10@hotmail.com>
2018-01-17 12:01:45 +0000

    Added capacity state to Airport class and updated land class to prevent a plane landing if airport at capacity

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -2,13 +2,31 @@ import java.util.ArrayList;
 
 public class Airport {
 
-    private ArrayList<Plane> planeArrayList = new ArrayList();
+    private ArrayList<Plane> planeArrayList;
+    private int capacity;
+
+    public Airport() {
+        this(5);
+        planeArrayList = new ArrayList<>();
+    }
+
+    public Airport(int capacity) {
+        this.capacity = capacity;
+        planeArrayList = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 
     public int GetNumberOfPlanes() {
         return planeArrayList.size();
     }
 
     public boolean land(Plane plane) {
+        if (GetNumberOfPlanes() == getCapacity()) {
+            return false;
+        }
         planeArrayList.add(plane);
         return true;
     }
--- src/test/groovy/AirportSpec.groovy
+++ src/test/groovy/AirportSpec.groovy
@@ -3,14 +3,27 @@ import spock.lang.Specification
 class AirportSpec extends Specification {
     def "An airport can receive a plane" () {
         given: "When an instance of an Airport is created"
-        def airport = new Airport();
-        def plane = Mock(Plane);
+        def airport = new Airport()
+        def plane = Mock(Plane)
 
         when: "An airport can receive a plane"
         airport.land(plane)
 
         then: "The plane is added to the Planes list"
         airport.GetNumberOfPlanes() == 1
+    }
+
+    def "An airport cannot receive a plane if at capacity"() {
+        given: "When an instance of an Airport is created"
+        def airport = new Airport(1)
+        def plane = Mock(Plane)
+        def plane2 = Mock(Plane)
+
+        when: "An airport with capacity of 1 has a plane land"
+        airport.land(plane)
+
+        then: "A plane cannot land"
+        !airport.land(plane2)
     }
 
 }
